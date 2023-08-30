ecc4495769836eb16b90f3d3002b1e8c3e9fb4d3
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-08-29 18:24:14 -0700

    fix issue with refined_post constructors

--- python_code/refined_post.py
+++ python_code/refined_post.py
@@ -2,35 +2,35 @@ import re
 
 
 class Refined_Post:
-    def __init__(self) -> None:
-        self.author = None
-        self.created = None
-        self.downs = None
-        self.edited = None
-        self.flair = None
-        self.locked = None
-        self.num_comments = None
-        self.score = None
-        self.text = None
-        self.title = None
-        self.num_awards = None
-        self.ups = None
-        self.ratio = None
-
-    def __init__(self, row):
-        self.author = row[0]
-        self.created = row[1]
-        self.flair = row[2]
-        self.title = row[3]
-        self.text = row[4]
-        self.score = row[4]
-        self.edited = row[6]
-        self.locked = row[7]
-        self.num_comments = row[8]
-        self.num_awards = row[9]
-        self.ups = row[10]
-        self.downs = row[11]
-        self.ratio = row[12]
+    def __init__(self, row=None):
+        if row == None:
+            self.author = None
+            self.created = None
+            self.downs = None
+            self.edited = None
+            self.flair = None
+            self.locked = None
+            self.num_comments = None
+            self.score = None
+            self.text = None
+            self.title = None
+            self.num_awards = None
+            self.ups = None
+            self.ratio = None
+        else:
+            self.author = row[0]
+            self.created = row[1]
+            self.flair = row[2]
+            self.title = row[3]
+            self.text = row[4]
+            self.score = row[4]
+            self.edited = row[6]
+            self.locked = row[7]
+            self.num_comments = row[8]
+            self.num_awards = row[9]
+            self.ups = row[10]
+            self.downs = row[11]
+            self.ratio = row[12]
 
     # Returns true if a word suggesting a romantic relationship is found
     def isRomantic(self) -> bool:
