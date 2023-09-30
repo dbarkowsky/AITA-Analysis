074ad39db28a9708efe5f79e6397759caef2d5c7
Dylan Barkowsky <dylanbarkowsky@gmail.com>
2023-09-29 21:00:11 -0700

    Add more data to StatsBundler class

--- python_code/run_analysis.py
+++ python_code/run_analysis.py
@@ -1,21 +1,25 @@
 # Questions to ask
 # 1. Are there frequent posters? Top 5? 10?
-# 2a. Most like to be the a-hole if posted at certain times of day? Morning, Afternoon, Evening, Night?
-# 2b. Maybe can't tell that considering that we don't know their timezones.
 # 3. What are the totals of each flair?
 # 4a. More likely to be the a-hole if edited?
 # 4b. Or are people editing their posts if flaired a-holes?
-# 5. More likely to be locked if a-hole?
 # 6. More likely to be a-hole if high comments? -> shows engagement
 # 7a. Are the a-holes more upvoted?
 # 7b. Are they getting more votes in general?
-# 7c. Do a-holes get downvoted more (despite not the intent of the downvote)?
 # 8. Is it more likely to be the a-hole if the age range is large? (original question)
 # 9. More likely to be a-hole if possibly romantic issue?
 # 10. More likely to be a-hole based on num of participants? 2, 3, 4+
 # 11. More likely to be a-hole based on age range? <18, 19-25, 26-40, 41+
 # 12. More likely to be a-hole based on gender?
 # 13. If only 2 participants, compare same vs different gender. For M and F.
+
+# Questions that might be out...
+# 2a. Most like to be the a-hole if posted at certain times of day? Morning, Afternoon, Evening, Night?
+# 2b. Maybe can't tell that considering that we don't know their timezones.
+# 5. More likely to be locked if a-hole?
+# 5a. Too few locked posts to tell...
+# 7c. Do a-holes get downvoted more (despite not the intent of the downvote)?
+# 7d. Seems like downvotes don't get captured
 
 import csv
 from stats_bundler import StatsBundler
@@ -55,6 +59,21 @@ with open(f"./combined_posts.csv", "r", encoding="utf-8") as file:
         stats.increment_flair(row.flair)
         # Sum of score for each flair
         stats.append_score(row.flair, row.score)
+        # Count genders per flair
+        stats.increment_gender(row.flair, row.gender)
+        # Count if edited (will be a number representing date, otherwise False string)
+        if row.edited != "False":
+            stats.increment_edited(row.flair)
+        # Count that age
+        if str(row.age).isdigit():
+            stats.increment_age_chunk(row.flair, row.age)
+            stats.increment_age(row.flair, row.age)
+        # Count age ranges
+        if str(row.age_range).isdigit():
+            stats.increment_age_range(row.flair, row.age_range)
+        # Count if romantic
+        if row.is_romantic == "True":
+            stats.increment_romantic(row.flair)
 
     # Find top 10 (11 including deleted) posters
     stats.top_10_posters()
@@ -64,3 +83,6 @@ with open(f"./combined_posts.csv", "r", encoding="utf-8") as file:
     # Median and Mean of each flair
     stats.flair_medians()
     stats.flair_means()
+
+    # Print A-hole count
+    stats.pretty_print_ahole_count()
--- python_code/stats_bundler.py
+++ python_code/stats_bundler.py
@@ -1,12 +1,28 @@
 import statistics
 from collections import defaultdict
+import copy
+import json
 
 
 class StatsBundler:
     def __init__(self):
+        default_dict = {
+            "gender": {"M": 0, "F": 0, "same": 0, "different": 0},
+            "edited": 0,
+            "age_chunk": {"<19": 0, "19-25": 0, "26-35": 0, "36-45": 0, ">45": 0},
+            "age": {},
+            "age_range": {},
+            "romantic": 0,
+        }
         self.frequent_posters = dict()
         self.flair_count = dict()
         self.flair_score_lists = defaultdict(list)
+        self.ahole_count = {
+            "Not the A-hole": copy.deepcopy(default_dict),
+            "Asshole": copy.deepcopy(default_dict),
+            "No A-holes here": copy.deepcopy(default_dict),
+            "Everyone Sucks": copy.deepcopy(default_dict),
+        }
 
     # Modifications
     def increment_poster(self, poster):
@@ -17,6 +33,40 @@ class StatsBundler:
 
     def append_score(self, flair, score):
         self.flair_score_lists[flair].append(int(score))
+
+    def increment_gender(self, flair, gender):
+        if ["M", "F"].__contains__(gender):
+            self.ahole_count[flair]["gender"][gender] += 1
+
+    def increment_edited(self, flair):
+        self.ahole_count[flair]["edited"] += 1
+
+    def increment_age_chunk(self, flair, age):
+        int_age = int(age)
+        match int_age:
+            case _ if int_age < 19:
+                self.ahole_count[flair]["age_chunk"]["<19"] += 1
+            case _ if int_age >= 19 and int_age <= 25:
+                self.ahole_count[flair]["age_chunk"]["19-25"] += 1
+            case _ if int_age >= 26 and int_age <= 35:
+                self.ahole_count[flair]["age_chunk"]["26-35"] += 1
+            case _ if int_age >= 36 and int_age <= 45:
+                self.ahole_count[flair]["age_chunk"]["36-45"] += 1
+            case _ if int_age >= 45:
+                self.ahole_count[flair]["age_chunk"][">45"] += 1
+
+    def increment_age(self, flair, age):
+        self.ahole_count[flair]["age"][str(age)] = (
+            self.ahole_count[flair]["age"].get(str(age), 0) + 1
+        )
+
+    def increment_age_range(self, flair, range):
+        self.ahole_count[flair]["age_range"][str(range)] = (
+            self.ahole_count[flair]["age_range"].get(str(range), 0) + 1
+        )
+
+    def increment_romantic(self, flair):
+        self.ahole_count[flair]["romantic"] += 1
 
     # Calculations
     def top_10_posters(self):
@@ -48,3 +98,7 @@ class StatsBundler:
                 f"{flair}: {round(statistics.mean(self.flair_score_lists[flair]), 2)}"
             )
         print()
+
+    # Utilities
+    def pretty_print_ahole_count(self):
+        print(json.dumps(self.ahole_count, sort_keys=True, indent=2))
